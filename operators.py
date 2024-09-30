754f5c8afc258fd1fdcc1890ff68074d37be9cd7
Vishesh Dua <visheshdua10@gmail.com>
2024-09-30 20:00:25 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,12 @@
+# input from user and multiply
+
+a = int(input('Enter number: '))
+b = int(input('Enter number: '))
+c = a*b
+print(c)
+
+# assignment operator
+a = 10
+a //= 10  
+# a = a//10
+print(a)
